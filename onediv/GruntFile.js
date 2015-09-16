84626364d2345d64410d1231d6301fea787fe084
ylp <yanliping@jd.com>
2015-09-16 19:18:37 +0800

    onediv

---
+++ onediv/GruntFile.js
@@ -0,0 +1,49 @@
+/**
+ * Created by zx000 on 2015/2/6.
+ */
+
+module.exports = function(grunt) {
+    'use strict';
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    style: "compressed"
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'scss/',
+                    src: ["**/*.scss"],
+                    dest: "css/",
+                    ext: '.css'
+                }]
+            }
+        },
+        watch: {
+            css: {
+                files: ['scss/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false
+                }
+            }
+        },
+        browserSync: {
+            bsFiles: {
+                src: ['./css/*.css', './*.html']
+            },
+            options: {
+                server: {
+                    baseDir: "./"
+                },
+                watchTask: true
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.registerTask('default', ['browserSync', 'watch:css']);
+};
